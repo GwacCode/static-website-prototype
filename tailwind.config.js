986670d0a1ca446fb7ab21ccc088896c52d2e424
Sean Hsien <umphy27@gmail.com>
2020-04-25 10:38:02 +1000

    update social icon colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
         "gw-blue-dark": "#2384a7",
         "gw-blue-darker": "#104354",
         "gw-yellow": "#f9d35b",
+        "facebook-blue": "#4267b2",
+        "vimeo-blue": "#00adef",
       },
       fontSize: {
         "7xl": "5rem",
