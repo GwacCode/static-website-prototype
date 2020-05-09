91284d94393427820debc04244ea67fc40368bef
Sean Hsien <umphy27@gmail.com>
2020-05-09 12:54:18 +1000

    update design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "gw-blue": "#008ad1",
+        "gw-blue": "#2990b5",
         "gw-blue-dark": "#2384a7",
         "gw-blue-darker": "#104354",
         "gw-yellow": "#f9d35b",
         "facebook-blue": "#4267b2",
         "vimeo-blue": "#00adef",
       },
-      fontSize: {
-        "7xl": "5rem",
-      },
+      // fontSize: {
+      //   "7xl": "5rem",
+      // },
       opacity: {
         "85": "0.85",
       },
